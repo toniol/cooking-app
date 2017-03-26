d060e43b0d2ca1e7933405d1ff40f7115c061a38
toniol <taoyingning2009@gmail.com>
2017-03-26 19:41:28 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import Vonic from 'vonic/src/index.js'
+import FastClick from 'fastclick'
 
 // routes
 import routes from './routes'
 
+// vuex
+import store from './vuex/'
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
 Vue.use(Vonic.app, {
-  routes: routes
+  routes,
+  store
 })
