04f882c7427f9a8bb0e6ad4909e5236c7d6b8895
toniol <taoyingning2009@gmail.com>
2017-04-20 16:00:21 +0800

    userinfo state

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,13 @@
-import { UPDATE_LOADING } from './mutation-types'
+import { UPDATE_LOADING, SAVED_USERINFO, REMOVE_USERINFO } from './mutation-types'
 
 export default {
-  [UPDATE_LOADING](state, status){
-    state.isLoading = status
-  }
+    [UPDATE_LOADING](state, status) {
+        state.isLoading = status
+    },
+    [SAVED_USERINFO](state, userinfo) {
+        state.userinfo = userinfo
+    },
+    [REMOVE_USERINFO](state) {
+        state.userinfo = null
+    }
 }
